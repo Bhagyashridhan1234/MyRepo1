0ea2134779c48e8c20cc3d0c1682a7294d0f7c50
bhagyashri123 <bhgyashridhanwate23@gmail.com>
2020-10-31 19:02:41 +0530

    first commit

---
+++ TestProjectInGitRepo2/src/main/java/com/example/demo/TestProjectInGitRepo2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectInGitRepo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectInGitRepo2Application.class, args);
+	}
+
+}
